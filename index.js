baa982eb8e25b1033916f9de6437dea3f1dc1e56
Rajneeshyadav987 <yraj03486@gmail.com>
2024-06-08 11:49:28 +0530

    let in javascript

---
+++ index.js
@@ -0,0 +1,64 @@
+/*//var
+var num =10;
+{
+    num =20;
+}
+console.log(num)
+
+//let
+let num1=10
+
+{
+    num1 =20
+}
+console.log("let",num1)
+
+//const
+const num2 =20
+
+{
+    num2 =20
+}
+console.log("const",num2)
+
+
+var num =10
+{
+    var num = 20
+    num = 30
+    console.log(num)
+}
+console.log(num)
+
+let num = 10
+{
+    let num1 = 20
+    num1 = 30
+    console.log(num1)
+}
+console.log(num1)
+
+const num2 = 10
+{
+    const num2 = 10
+
+    console.log(num2)
+}
+console.log(num2)*/
+
+let num1 =10;
+let num2 = 20;
+
+let  sum = num1 + num2;
+console.log(sum)
+
+let multiply = num1 * num2;
+console.log(multiply)
+
+let devide = num1/num2;
+console.log(devide)
+
+let a =10;
+let b = 25;
+sum = a++ + ++b ;
+console.log(sum) 
